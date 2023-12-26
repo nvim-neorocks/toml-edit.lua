f19445379f1f95dfcda7a9ae43c820484c2bde0d
Marc Jakobi <marc.jakobi@tiko.energy>
2023-12-26 20:34:49 +0100

    ci(nix): add simple spec + nix flake checks + github workflow

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -6,6 +6,24 @@
       cargoDeps = final.rustPlatform.importCargoLock {
         lockFile = self + "/Cargo.lock";
       };
+      nativeCheckInputs = with luaself;
+        [
+          luarocks-build-rust-mlua
+          busted
+        ]
+        ++ oa.nativeCheckInputs;
+      propagatedBuildInputs =
+        # HACK: This shouldn't be necessary, but it appears to be.
+        with luaself;
+          [
+            busted
+          ]
+          ++ oa.propagatedBuildInputs;
+      preCheck = ''
+        mkdir luarocks
+        luarocks $LUAROCKS_EXTRA_ARGS make --tree=luarocks --deps-mode=all
+      '';
+      doCheck = true;
     });
   };
   lua5_1 = prev.lua5_1.override {
