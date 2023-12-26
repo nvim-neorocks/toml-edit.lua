4777aebfa3ea8d3a43ffbf5958a2f603379ec6bc
Marc Jakobi <marc.jakobi@tiko.energy>
2023-12-26 01:52:16 +0100

    build(nix): add nix flake with devShell and overlay

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  description = "devShell for Rust projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    pre-commit-hooks,
+    ...
+  }: let
+    overlay = import ./nix/overlay.nix {inherit self;};
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [overlay];
+        };
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = self;
+          hooks = {
+            alejandra.enable = true;
+            rustfmt.enable = true;
+          };
+        };
+      in {
+        packages.default = pkgs.lua51Packages.toml-edit;
+
+        devShells.default = pkgs.lua51Packages.toml-edit.overrideAttrs (oa: {
+          name = "toml-edit devShell";
+
+          buildInputs =
+            (with pkgs; [
+              rust-analyzer
+            ])
+            ++ (with pre-commit-hooks.packages.${system}; [
+              alejandra
+              rustfmt
+            ])
+            ++ oa.buildInputs;
+
+          shellHook = ''
+            ${oa.shellHook}
+            ${self.checks.${system}.pre-commit-check.shellHook}
+          '';
+
+          doCheck = false;
+        });
+
+        checks = {
+          inherit pre-commit-check;
+          inherit (pkgs.lua51Packages) toml-edit;
+        };
+      };
+      flake = {
+        overlays.default = overlay;
+      };
+    };
+}
