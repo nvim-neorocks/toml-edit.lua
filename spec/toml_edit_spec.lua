c95f47046e080503ed9cddc8a3b8a3bd89e4ec2f
Marc Jakobi <marc.jakobi@tiko.energy>
2023-12-26 20:36:20 +0100

    docs(readme): init

--- spec/toml_edit_spec.lua
+++ spec/toml_edit_spec.lua
@@ -1,10 +1,10 @@
 describe("toml-edit", function()
+    local toml_edit = require("toml_edit")
     it("Can read from key", function()
         local toml_content = [[
           [rocks]
           "toml-edit" = "1.0.0"
         ]]
-        local toml_edit = require("toml_edit")
         local result = toml_edit.parse(toml_content)
         assert.equal("1.0.0", result.rocks["toml-edit"])
     end)
@@ -13,7 +13,17 @@ describe("toml-edit", function()
           [rocks]
           "toml-edit" = "%s"
         ]]
-        local toml_edit = require("toml_edit")
+        local result = toml_edit.parse(toml_content)
+        result.rocks["toml-edit"] = "1.0.0"
+        local expected = toml_content:format("1.0.0")
+        assert.equal(expected, tostring(result))
+    end)
+    it("Preserves comments", function()
+        local toml_content = [[
+          [rocks]
+          # Some comment
+          "toml-edit" = "%s"
+        ]]
         local result = toml_edit.parse(toml_content)
         result.rocks["toml-edit"] = "1.0.0"
         local expected = toml_content:format("1.0.0")
