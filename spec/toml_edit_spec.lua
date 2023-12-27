f6731821282dbead2425215a82f3adec42fa71ac
Marc Jakobi <marc.jakobi@tiko.energy>
2023-12-28 00:26:46 +0100

    fix: support inline tables

--- spec/toml_edit_spec.lua
+++ spec/toml_edit_spec.lua
@@ -8,6 +8,35 @@ describe("toml-edit", function()
         local result = toml_edit.parse(toml_content)
         assert.equal("1.0.0", result.rocks["toml-edit"])
     end)
+    it("Can read from table", function()
+        local toml_content = [[
+          [rocks.neorg]
+          version = "1.0.0"
+          opt = true
+        ]]
+        local result = toml_edit.parse(toml_content)
+        local rock = result.rocks.neorg
+        assert.equal("1.0.0", rock.version)
+        assert.equal(true, rock.opt)
+    end)
+    it("Can read from inline table", function()
+        local toml_content = [[
+          [rocks]
+          "toml-edit" = { version = "1.0.0", opt = true }
+        ]]
+        local result = toml_edit.parse(toml_content)
+        local rock = result.rocks["toml-edit"]
+        assert.equal("1.0.0", rock.version)
+        assert.equal(true, rock.opt)
+    end)
+    it("Preserves inline tables", function()
+        local toml_content = [[
+          [rocks]
+          "toml-edit" = { version = "1.0.0", opt = true }
+        ]]
+        local result = toml_edit.parse(toml_content)
+        assert.equal(toml_content, tostring(result))
+    end)
     it("Can set value", function()
         local toml_content = [[
           [rocks]
@@ -27,6 +56,55 @@ describe("toml-edit", function()
         local result = toml_edit.parse(toml_content)
         result.rocks["toml-edit"] = "1.0.0"
         local expected = toml_content:format("1.0.0")
+        assert.equal(expected, tostring(result))
+    end)
+    it("Can set value in table", function()
+        local toml_content = [[
+          [rocks.neorg]
+          version = "%s"
+        ]]
+        local result = toml_edit.parse(toml_content)
+        local rock = result.rocks.neorg
+        rock.version = "2.0.0"
+        local expected = toml_content:format("2.0.0")
+        assert.equal(expected, tostring(result))
+    end)
+    it("Can add value to table", function()
+        local toml_content = [[
+[rocks.neorg]
+version = "1.0.0"
+]]
+        local result = toml_edit.parse(toml_content)
+        local rock = result.rocks.neorg
+        rock.opt = false
+        local expected = [[
+[rocks.neorg]
+version = "1.0.0"
+opt = false
+]]
+        assert.equal(expected, tostring(result))
+    end)
+    it("Preserves inline tables when setting a value", function()
+        local toml_content = [[
+[rocks]
+"toml-edit" = { version = "%s" }
+]]
+        local result = toml_edit.parse(toml_content)
+        result.rocks["toml-edit"].version = "2.0.0"
+        local expected = toml_content:format("2.0.0")
+        assert.equal(expected, tostring(result))
+    end)
+    it("Preserves inline tables when adding a new value", function()
+        local toml_content = [[
+[rocks]
+"toml-edit" = { version = "1.0.0" }
+]]
+        local result = toml_edit.parse(toml_content)
+        result.rocks["toml-edit"].opt = true
+        local expected = [[
+[rocks]
+"toml-edit" = { version = "1.0.0" , opt = true }
+]]
         assert.equal(expected, tostring(result))
     end)
 end)
