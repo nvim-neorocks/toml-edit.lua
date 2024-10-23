6822aa676e4df28766b07c2b97eb9799488b6664
Simi Falaye <simifalaye1@gmail.com>
2024-10-23 12:44:22 -0600

    feat: Add support for toml arrays

--- src/lib.rs
+++ src/lib.rs
@@ -13,17 +13,164 @@ where
     let table = lua.create_table()?;
     table.set("__path", Vec::<String>::new())?;
 
-    let metatable = lua.create_table()?;
-    let mt_clone = metatable.clone();
+    let list_metatable = lua.create_table()?;
+    let list_metatable_clone = list_metatable.clone();
+    let list_index_document = Rc::clone(&document);
+    list_metatable.set(
+        "__index",
+        lua.create_function(move |lua, (tbl, key): (mlua::Table, mlua::Integer)| {
+            let path = tbl.get::<_, Vec<String>>("__path")?;
 
-    let index_document_copy = Rc::clone(&document);
-    metatable.set(
+            let binding = list_index_document.borrow();
+            let entry = path.clone().into_iter().try_fold(
+                binding.as_item(),
+                |entry, next_key: String| {
+                    Ok::<_, mlua::Error>(
+                        entry
+                            .as_table_like()
+                            .ok_or_else(|| {
+                                mlua::Error::RuntimeError(
+                                    "attempt to index '".to_string()
+                                        + next_key.as_str()
+                                        + "' (a "
+                                        + entry.type_name()
+                                        + " value)",
+                                )
+                            })?
+                            .get(next_key.as_str())
+                            .unwrap_or_else(|| &toml_edit::Item::None),
+                    )
+                },
+            )?;
+
+            match entry {
+                toml_edit::Item::Value(value) => match value {
+                    toml_edit::Value::Array(array) => match array.get((key - 1) as usize) {
+                        Some(array_value) => match array_value {
+                            toml_edit::Value::String(str) => Ok(lua.to_value(&str.value())),
+                            toml_edit::Value::Integer(int) => Ok(lua.to_value(&int.value())),
+                            toml_edit::Value::Float(float) => Ok(lua.to_value(&float.value())),
+                            toml_edit::Value::Boolean(bool) => Ok(lua.to_value(&bool.value())),
+                            _ => Err(mlua::Error::RuntimeError(format!(
+                                "toml-edit: cannot parse {}: {} yet",
+                                array_value.type_name(),
+                                array_value
+                            ))),
+                        },
+                        None => Ok(Ok(Value::Nil)),
+                    },
+                    _ => Err(mlua::Error::RuntimeError(format!(
+                        "toml-edit: expected value to be an array type {}: {}",
+                        value.type_name(),
+                        value
+                    ))),
+                },
+                item => Err(mlua::Error::RuntimeError(format!(
+                    "toml-edit: expected item to be a value type, got {}: {}",
+                    item.type_name(),
+                    item
+                ))),
+            }
+        })?,
+    )?;
+
+    let list_newindex_document = Rc::clone(&document);
+    list_metatable.set(
+        "__newindex",
+        lua.create_function(
+            move |_, (tbl, key, value): (mlua::Table, mlua::Integer, Value)| {
+                let path = tbl.get::<_, Vec<String>>("__path")?;
+
+                let mut binding = list_newindex_document.borrow_mut();
+                let entry: &mut toml_edit::Item =
+                    path.clone()
+                        .into_iter()
+                        .fold(binding.as_item_mut(), |entry, next_key| {
+                            if !entry.is_table_like() {
+                                *entry = toml_edit::Item::Table(toml_edit::Table::default());
+                            };
+
+                            let entry = entry.as_table_like_mut().unwrap();
+
+                            if entry.get_mut(next_key.as_str()).is_none() {
+                                entry.insert(
+                                    next_key.as_str(),
+                                    toml_edit::Item::Table(toml_edit::Table::default()),
+                                );
+                            }
+
+                            entry.get_mut(next_key.as_str()).unwrap()
+                        });
+
+                match entry {
+                    toml_edit::Item::Value(entry_value) => match entry_value {
+                        toml_edit::Value::Array(array) => {
+                            let toml_value = match value {
+                                Value::String(str) => toml_edit::value(str.to_str()?.to_string()),
+                                Value::Number(number) => toml_edit::value(
+                                    lua.from_value::<mlua::Number>(Value::Number(number))?,
+                                ),
+                                Value::Integer(int) => toml_edit::value(
+                                    lua.from_value::<mlua::Integer>(Value::Integer(int))?,
+                                ),
+                                Value::Boolean(bool) => toml_edit::value(bool),
+                                _ => {
+                                    return Err(mlua::Error::MetaMethodTypeError {
+                                        method: "__newindex".into(),
+                                        type_name: value.type_name(),
+                                        message: Some(
+                                            "provided type is not allowed in TOML document".into(),
+                                        ),
+                                    })
+                                }
+                            };
+                            let index = (key - 1) as usize;
+                            let array_value_opt = array.get_mut(index);
+                            if let Some(array_value) = array_value_opt {
+                                *array_value = toml_value.as_value().unwrap().clone();
+                            } else {
+                                array.insert(index, toml_value.as_value().unwrap());
+                            }
+                            return Ok(());
+                        }
+                        _ => {
+                            return Err(mlua::Error::MetaMethodTypeError {
+                                method: "__newindex".into(),
+                                type_name: value.type_name(),
+                                message: Some(format!(
+                                    "entry value is not a list type in toml document, {}: {}",
+                                    entry_value.type_name(),
+                                    entry_value
+                                )),
+                            })
+                        }
+                    },
+                    item => {
+                        return Err(mlua::Error::MetaMethodTypeError {
+                            method: "__newindex".into(),
+                            type_name: value.type_name(),
+                            message: Some(format!(
+                                "item is not a value type in toml document, {}: {}",
+                                item.type_name(),
+                                item
+                            )),
+                        })
+                    }
+                }
+            },
+        )?,
+    )?;
+
+    let table_metatable = lua.create_table()?;
+    let table_metatable_clone = table_metatable.clone();
+    let table_index_document = Rc::clone(&document);
+    table_metatable.set(
         "__index",
         lua.create_function(move |lua, (tbl, key): (mlua::Table, mlua::String)| {
             let mut path = tbl.get::<_, Vec<String>>("__path")?;
             path.push(key.to_str()?.to_string());
 
-            let binding = index_document_copy.borrow();
+            let binding = table_index_document.borrow();
             let entry = path.clone().into_iter().try_fold(
                 binding.as_item(),
                 |entry, next_key: String| {
@@ -49,7 +196,7 @@ where
                 toml_edit::Item::Table(_) => {
                     let table = lua.create_table()?;
                     table.set("__path", path)?;
-                    table.set_metatable(Some(mt_clone.clone()));
+                    table.set_metatable(Some(table_metatable_clone.clone()));
 
                     Ok(Value::Table(table))
                 }
@@ -61,7 +208,13 @@ where
                     toml_edit::Value::InlineTable(_) => {
                         let table = lua.create_table()?;
                         table.set("__path", path)?;
-                        table.set_metatable(Some(mt_clone.clone()));
+                        table.set_metatable(Some(table_metatable_clone.clone()));
+                        Ok(Value::Table(table))
+                    }
+                    toml_edit::Value::Array(_) => {
+                        let table = lua.create_table()?;
+                        table.set("__path", path)?;
+                        table.set_metatable(Some(list_metatable_clone.clone()));
                         Ok(Value::Table(table))
                     }
                     _ => Err(mlua::Error::RuntimeError(format!(
@@ -79,15 +232,15 @@ where
         })?,
     )?;
 
-    let newindex_document_copy = Rc::clone(&document);
-    metatable.set(
+    let table_newindex_document = Rc::clone(&document);
+    table_metatable.set(
         "__newindex",
         lua.create_function(
             move |_, (tbl, key, value): (mlua::Table, mlua::String, Value)| {
                 let mut path = tbl.get::<_, Vec<String>>("__path")?;
                 path.push(key.to_str()?.to_string());
 
-                let mut binding = newindex_document_copy.borrow_mut();
+                let mut binding = table_newindex_document.borrow_mut();
                 let entry: &mut toml_edit::Item =
                     path.clone()
                         .into_iter()
@@ -144,12 +297,12 @@ where
         )?,
     )?;
 
-    metatable.set(
+    table_metatable.set(
         "__tostring",
         lua.create_function(move |lua, ()| lua.to_value(&document.borrow().to_string()))?,
     )?;
 
-    table.set_metatable(Some(metatable.clone()));
+    table.set_metatable(Some(table_metatable.clone()));
 
     Ok(table)
 }
